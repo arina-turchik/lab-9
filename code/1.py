056d4c076b0d0a40c82b2433ada65da786b3ec78
kzar00 <kzar000@mail.ru>
2024-12-26 14:05:31 +0500

    upd code

--- code/1.py
+++ code/1.py
@@ -1,14 +1,15 @@
-def random_number(s, minn, maxx):
-    # Линейный конгруэнтный генератор
-    a = 1664525
-    c = 1013904223
+def random_number(s, minn, maxx, i):
+    a = 1664525 + i*93876
+    c = 1013904223 *i//83746345
     m = 2**32
     s = (a * s + c) % m
-    return minn + (s % (maxx - minn + 1))
-print('Введите любое число, начальное число диапазона и конечное число диапазона:')
-s = int(input())
-minn = int(input())
-maxx = int(input())
+    return minn + (s % (maxx - minn + 1)), i + 1
 
-random_n = random_number(s, minn, maxx)
-print(random_n)
+s = 77
+minn = 1409
+maxx = 1489762
+
+i = 0
+for r in range(10):
+    random_n, i = random_number(s, minn, maxx, i)
+    print(random_n)
