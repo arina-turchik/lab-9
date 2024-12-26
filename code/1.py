1dd748eebd20d42853483308cd0e8f882ec4c180
kzar00 <kzar000@mail.ru>
2024-12-26 14:31:12 +0500

    AAAAAAAAA

--- code/1.py
+++ code/1.py
@@ -1,15 +1,18 @@
-def random_number(s, minn, maxx, i):
-    a = 1664525 + i*93876
-    c = 1013904223 *i//83746345
-    m = 2**32
-    s = (a * s + c) % m
-    return minn + (s % (maxx - minn + 1)), i + 1
+def random_number(s, minn, maxx):
+    i = 0
+    m = 2**32 
+    while True:
+        a = 1664525 + i * 93876
+        c = 1013904223
+        s = (a * s + c) % m
+        yield minn + (s % (maxx - minn + 1))
+        i += 1
 
-s = 77
-minn = 1409
-maxx = 1489762
+s = 47
+minn = 489
+maxx = 594437
 
-i = 0
+gen = random_number(s, minn, maxx)
 for r in range(10):
-    random_n, i = random_number(s, minn, maxx, i)
+    random_n = next(gen)
     print(random_n)
